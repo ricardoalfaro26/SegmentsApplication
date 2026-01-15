9a775c7410abaf665ddee5d1c9e344e3b1cfa225
Ricardo Enrique Alfaro Rodriguez <ricardo.alfaro@integral.com.sv>
2026-01-15 14:01:14 -0600

    Proyecto de segmentos

---
+++ SegmentsFormBackend.Api/Controllers/RetiredCustomerSegmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SegmentsFormBackend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RetiredCustomerSegmentController : ControllerBase
+    {
+    }
+}
